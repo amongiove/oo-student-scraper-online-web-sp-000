2f6790ce0e98a0de8720c804e26de2cc5a8c26f9
Amanda Mongiove <amandamongiove@gmail.com>
2020-10-08 22:24:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,7 @@ class Student
   @@all = []
 
   def initialize(attributes)
-    attributes.each {|key, value| self.send (("#{key}="), value)}
+    attributes.each {|key, value| self.send (("#{key}="),value)}
     @@all << self
   end
 
