91964f27f8ab2304e68853fb380fd67f2e56179e
Amanda Mongiove <amandamongiove@gmail.com>
2020-10-08 22:40:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,7 +24,7 @@ class Student
     Self.new(attributes_hash.each do |key, value|
       self.send("#{key}=", value) if self.respond_to?("#{key}=")
     end)
-    binding.pry
+    # binding.pry
   end
 
 end
